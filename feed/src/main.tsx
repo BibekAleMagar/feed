960e072b46aae82369e90330c71abe5bd4e926ef
Bibek Ale Magar <magarbibek71@gmail.com>
2025-08-03 14:00:42 +0545

    r

--- feed/src/main.tsx
+++ feed/src/main.tsx
@@ -1,18 +1,12 @@
-import { StrictMode, useEffect } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import { queryClient } from './lib/react-query.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const redirectUrl = localStorage.getItem('ghpages-redirect');
-if (redirectUrl) {
-  localStorage.removeItem('ghpages-redirect');
-  const newUrl = '/feed' + (redirectUrl.startsWith('/') ? redirectUrl : '/' + redirectUrl);
-  window.history.replaceState(null, '', newUrl);
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -22,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 )

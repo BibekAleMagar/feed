d85754605e6b5b43a4cb3e077255ab42a7bdf99b
Bibek Ale Magar <magarbibek71@gmail.com>
2025-08-03 13:25:05 +0545

    /final

--- feed/vite.config.ts
+++ feed/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   server: {host: true},
-  base: '/',
+  base: '/feed/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
